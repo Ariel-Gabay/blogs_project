d22ee6c6f98d6a993e8085944082b35b8e8107a6
Ariel Gabay <hag.lib.ada@gmail.com>
2025-12-03 20:03:18 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import PostEditor from "@/lib/components/PostEditor";
 import Categories from "./Categories";
 
 const Home = () => {
   return (
     <>
       <Categories />
-      <PostEditor id={null} />
     </>
   );
 };
