4b7195ce90d560093d433319514dcd27ce20536d
Ariel Gabay <hag.lib.ada@gmail.com>
2025-12-11 00:23:29 +0200

    //

--- lib/supabase/clientServer.ts
+++ lib/supabase/clientServer.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  "https://wnifthoolaprtfaaclbq.supabase.co",
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: {
